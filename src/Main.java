662cd7902beb196b714dfb281726ab60f239bc9d
laila <laila@fake.com>
2017-09-10 13:24:56 +0300

    partially working xpath parser that crashes at malformed html

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@
  * Created by Liuda on 8/5/2017.
  */
 
-//import
+import java.io.StringReader;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.xml.sax.InputSource;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world");
 
         try {
-            String page = RequestHTTP.getHTML("https://dexonline.ro");
+            String page = RequestHTTP.getHTML("https://dexonline.ro/cuvantul-zilei");
             System.out.println(page);
+
+            XPath xxpath = XPathFactory.newInstance().newXPath();
+            InputSource inputSource = new InputSource(new StringReader(page));
+            String query = "//span[@class=\"def\"]/b[1]/text()";
+            String result = (String) xxpath.evaluate(query, inputSource, XPathConstants.STRING);
+            System.out.println(result);
+
+
         }
         catch(Exception error) {
             System.out.println("An error occurred while retrieving the page\n" + error.toString());
