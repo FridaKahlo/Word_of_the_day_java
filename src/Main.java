c05c1655c93ca22744c3019b82b2a418491a8a38
laila <laila@fake.com>
2017-09-10 12:14:13 +0300

    Added Get HTTP Request

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+package Word_of_the_day;
+
 /**
  * Created by Liuda on 8/5/2017.
  */
+
+//import
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world");
+        String page = RequestHTTP.getHTML("http://dexonline.ro");
+        System.out.println(page);
     }
 }
---
+++ src/RequestHTTP.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+
+
+public class RequestHTTP {
+    //taken from https://stackoverflow.com/a/1359702
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+}
