4429ba383a28db70897711fc76b5083074faa505
laila <laila@fake.com>
2017-09-10 13:47:32 +0300

    extracting image url of the word

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Created by Liuda on 8/5/2017.
  */
@@ -8,8 +13,16 @@ public class Main {
         System.out.println("hello world");
 
         try {
-            String page = RequestHTTP.getHTML("https://dexonline.ro");
+            String page = RequestHTTP.getHTML("https://dexonline.ro/cuvantul-zilei");
             System.out.println(page);
+
+            Document doc = Jsoup.parse(page);
+
+            String query_image = "img[class=img-responsive center-block]";
+            Elements image_url_node = doc.select(query_image);
+            String image_url = image_url_node.attr("src");
+            System.out.println(image_url);
+
         }
         catch(Exception error) {
             System.out.println("An error occurred while retrieving the page\n" + error.toString());
